08f8ba709ec0ca7611a6123998808cd856ba7fdf
Rasim Aghayev <rasimaqayev@gmail.com>
2023-07-31 04:13:31 +0400

    20. Add Profile Experience

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -173,4 +173,52 @@ router.delete('/', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @router  PUT     api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put(
+  '/experience',
+  [
+    auth,
+    check('title', 'Title is required.').not().isEmpty(),
+    check('company', 'Company is required.').not().isEmpty(),
+    check('from', 'From date is required.').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      return res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).json({ errors: [{ msg: 'Server error' }] });
+    }
+  }
+);
+
 module.exports = router;
