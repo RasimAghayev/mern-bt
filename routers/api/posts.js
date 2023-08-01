3e51fb2e2b57f712079febe78b7f73af2aeff097
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2023-08-01 14:09:26 +0000

    25. Add Post Route

--- routers/api/posts.js
+++ routers/api/posts.js
@@ -1,10 +1,49 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+// Middleware
+const auth = require('../../middleware/auth');
+
+// Models
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { config } = require('process');
+
+// Router
 const router = express.Router();
 
-// @router  GET     api/posts
-// @desc    Test    route
+// @router  POST    api/posts
+// @desc    Create a post
 // @access  Public
 
-router.get('/', (req, res) => res.send('Posts route.'));
+router.post('/', 
+[
+  auth,
+  check('text', 'Text is required.').not().isEmpty(),
+],
+async (req, res) => {
+    const errors=validationResult(req)
+    if(!errors){
+        return res.status(400).json({ errors: errors.array()})
+    }
+
+    const user=await User.findById(req.user.id).select(-passwrod)
+    try {
+
+        const newPost=new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+        
+        const post= await newPost.save()
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error.')
+    }
+});
 
 module.exports = router;
