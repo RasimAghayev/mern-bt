6b606b802f61235c3c1bb76749889e8dd4e32a7e
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2023-08-04 14:00:04 +0000

    35. Request Example & Login Form

--- fe/src/components/auth/Login.js
+++ fe/src/components/auth/Login.js
@@ -1,8 +1,77 @@
-import React from 'react'
+import React,{Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
+// import axios from 'axios'
 
 const Login = () => {
+  const [formData,setFromData]=useState({
+    email: '',
+    password: ''
+  });
+
+  const {email,password}=formData
+  const onChange=e=>setFromData({...formData, [e.target.name]: e.target.value})
+
+  const onSubmit = async e =>{
+    e.preventDefault(); 
+    // if(password!==password2){
+    //   console.error('Passwords do not match.')
+    // }else{
+      console.log('SUCCESS.')
+    // }
+    /*const newUser={
+      name,
+      email,
+      password
+    }
+    try {
+      const config={
+        headers:{
+          'Content-Type':'application/json'
+        }
+      }
+      const body=JSON.stringify(newUser)
+      const res=await axios.post('api/users',body,config)
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response.data)
+    }*/
+
+  }
   return (
-    <div>Login</div>
+    <Fragment>
+    <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={e=>onSubmit(e)}>
+        <div className="form-group">
+          <input 
+              type="email" 
+              placeholder="Email Address" 
+              name="email" 
+              value={email}
+              onChange={e=>onChange(e)}
+              required 
+            />
+          <small className="form-text" >
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e=>onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
 
--- fe/src/components/auth/Register.js
+++ fe/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React,{Fragment, useState} from 'react'
+import {Link} from 'react-router-dom'
+// import axios from 'axios'
 
 const Register = () => {
   const [formData,setFromData]=useState({
@@ -11,13 +13,31 @@ const Register = () => {
   const {name,email,password,password2}=formData
   const onChange=e=>setFromData({...formData, [e.target.name]: e.target.value})
 
-  const onSubmit = e =>{
+  const onSubmit = async e =>{
     e.preventDefault(); 
     if(password!==password2){
       console.error('Passwords do not match.')
     }else{
-      console.error(formData)
+      console.log('SUCCESS.')
     }
+    /*const newUser={
+      name,
+      email,
+      password
+    }
+    try {
+      const config={
+        headers:{
+          'Content-Type':'application/json'
+        }
+      }
+      const body=JSON.stringify(newUser)
+      const res=await axios.post('api/users',body,config)
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response.data)
+    }*/
+
   }
   return (
     <Fragment>
@@ -71,7 +91,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   )
