dbd5f67b8cc7bb86dc1a33a8213aef94cdb93f08
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2023-08-10 17:32:04 +0400

    40. Auth Reducer & Register Action

--- fe/src/components/auth/Register.js
+++ fe/src/components/auth/Register.js
@@ -2,11 +2,12 @@ import React,{Fragment, useState} from 'react'
 import {connect } from 'react-redux'
 import {Link} from 'react-router-dom'
 import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
 import PropTypes from 'prop-types'
 
 // import axios from 'axios'
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert,register}) => {
   const [formData,setFromData]=useState({
     name: '',
     email: '',
@@ -22,7 +23,7 @@ const Register = ({setAlert}) => {
     if(password!==password2){
       setAlert('Passwords do not match.','danger',3000)
     }else{
-      console.log('SUCCESS.')
+      register({name,email,password})
     }
     /*const newUser={
       name,
@@ -103,6 +104,10 @@ const Register = ({setAlert}) => {
 
 Register.PropTypes={
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 }
 
-export default connect(null, {setAlert})(Register)
+export default connect(
+    null, 
+    {setAlert,register}
+  )(Register)
