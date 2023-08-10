4e2c29d99b23fbddf48d726f8702f99fe591d2d0
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2023-08-10 17:59:28 +0400

    42. User Login part 1

--- fe/src/actions/auth.js
+++ fe/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { REGISTER_FAIL, REGISTER_SUCCESS,AUTH_ERROR,USER_LOADED } from "./types";
+import { REGISTER_FAIL, REGISTER_SUCCESS,LOGIN_FAIL, LOGIN_SUCCESS,AUTH_ERROR,USER_LOADED } from "./types";
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -22,6 +22,7 @@ export const loadUser=()=> async dispatch=>{
     }
 }
 
+//Register
 export const register =({ name, email, password}) => async dispatch=>{
     const config ={
         headers:{
@@ -46,5 +47,31 @@ export const register =({ name, email, password}) => async dispatch=>{
         })
         
     }
-    setAlert
+}
+
+//Login 
+export const login =({email, password}) => async dispatch=>{
+    const config ={
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }
+    const body=JSON.stringify({email,password})
+
+    try {
+        const res =await axios.post('/api/auth',body,config)
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+    } catch (err) {
+        const errors=err.response.data.errors
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg,'danger')));
+        }
+        dispatch({
+            type: LOGIN_FAIL
+        })
+        
+    }
 }
--- fe/src/reducers/auth.js
+++ fe/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { AUTH_ERROR, REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED } from "../actions/types";
+import { AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS,REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED } from "../actions/types";
 
 const initialState={
     token: localStorage.getItem('token'),
@@ -18,6 +18,7 @@ export default function(state =initialState,action){
                 user: payload
             }
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token',payload.token)
             return {
                 ...state,
@@ -26,6 +27,7 @@ export default function(state =initialState,action){
                 loading: false
             }
         case REGISTER_FAIL:
+        case LOGIN_FAIL:
         case AUTH_ERROR:
             localStorage.removeItem('token')
             return {
